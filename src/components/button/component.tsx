ee13363f647f09d1ea737db41b256515f30d5c75
Galiulin.Nik <galiulin.nik@gmail.com>
2023-08-27 18:34:07 +0600

    Login form ready

---
+++ src/components/button/component.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+export const Button = ({ text }: { text: string }) => {
+  return <button>{text}</button>;
+};
---
+++ src/components/input-text/component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export const InputText = ({
+  type = "text",
+  placeholder,
+}: {
+  type: string;
+  placeholder?: string;
+}) => {
+  return <input className="InputText" type={type} placeholder={placeholder} />;
+};
